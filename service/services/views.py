4ead8324af16bebc5449035d05de06b0cf8c2d5a
Iliukuhin Ilia <iiliukhin@gmail.com>
2023-03-10 13:46:54 +0400

    optimized view

--- service/services/views.py
+++ service/services/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from rest_framework.viewsets import ReadOnlyModelViewSet
 from services.serializers import SubscriptionSerializer
 from services.models import Subscription, Client
-from django.db.models import Prefetch
+from django.db.models import Prefetch, F, Sum
+
 
 class SubscriptionView(ReadOnlyModelViewSet):
     queryset = Subscription.objects.all().prefetch_related(
@@ -10,5 +11,15 @@ class SubscriptionView(ReadOnlyModelViewSet):
         Prefetch('client', queryset=Client.objects.all()
         .select_related('user')
         .only('company_name', 'user__email'))
-    )
+    ).annotate(price=F('service__full_price') -
+                     F('service__full_price') * F('plan__discount_percent') / 100.00)
     serializer_class = SubscriptionSerializer
+
+    def list(self, request, *args, **kwargs):
+        """Переопределение метода  list для получения суммы всех подписок"""
+        queryset = self.filter_queryset(self.get_queryset())
+        response = super().list(request, *args, **kwargs)
+        response_data = {'result': response.data}
+        response_data['total_amount'] = queryset.aggregate(total=Sum('price')).get('total')
+        response.data = response_data
+        return response
